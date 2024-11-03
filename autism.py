30dac8ef0fe99a5851ed276faf8df8e3490378b2
athirah syazwani <93869166+tirasyaz@users.noreply.github.com>
2024-11-03 12:46:15 +0800

    Update autism.py

--- autism.py
+++ autism.py
@@ -1,1 +1,9 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+df = pd.read_csv('https://raw.githubusercontent.com/tirasyaz/autism_prediction/refs/heads/main/Toddler%20Autism%20dataset%20July%202018.csv')
+
+df.head()
+
+st.write(df)
 
